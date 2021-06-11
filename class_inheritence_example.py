91034733319ef571c7011471160f508dcb546e60
David <dcmswim@gmail.com>
2021-06-11 09:15:47 -0700

    Update class_inheritence_example.py
    changed parent/child classes to better suit assignment guidelines

--- class_inheritence_example.py
+++ class_inheritence_example.py
@@ -1,16 +1,26 @@
 
 
-class Player:
-    Name = "User provided"
-    Health = 100
-    Armor = 10
+#Parent class
+class Employee:
+    name = "Unknown" #input when employee is hired
+    email = "Unknown" #created at time of hire
+    Pay_scale = 0 #This would be adjusted accordingly in child classes
+    Weekly_hours = 40 #This could also be adjusted accordingly for child classes
+    bonus_eligible = False
+    
 
-class Fighter(Player)
-    Health_modifier = Health * 1.5
-    Armor_modifer = Armor * 1.25
-    Stamina = 50
-    Attack_power = 25
+#child class
+class Manager(Employee):
+    Pay_scale = 5
+    Weekly_hours = 50
+    bonus_eligible = True
+    unlock_ID = 12345
+    expense_allowance = 250.00
 
-class Wizard(Player)
-    Mana = 100
-    Spell_power = 40
+
+#another child class
+class Driver(Employee):
+    Pay_scale = 3
+    Truck_ID = 9876
+    CDL_license = ABC
+    fuel_allocation = 100.00
