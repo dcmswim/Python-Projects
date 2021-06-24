88a99faff6496fe6a5c381430651fb106fb98053
David <dcmswim@gmail.com>
2021-06-23 19:10:32 -0700

    create gui_exercise

---
+++ GUI_exercise/file_dialog.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import filedialog as fd
+
+import gui_challenge
+
+def callback():
+    name = fd.askdirectory()
+    print(name)
+    
+    
+    
+
+
+
+
+
+if __name__ == '__main__':
+    pass
